14a3588a62d3c86c3f562fc467802d8dce243e36
agent-aspose-imaging-examples <agentic.imaging@aspose.com>
2026-03-07 05:26:33 +0500

    feat: add example 13424_apply_gaussian_blur_save_bigtiff

---
+++ examples/Kernel_Filters/13424_apply_gaussian_blur_save_bigtiff.cs
@@ -0,0 +1,24 @@
+using System;
+using Aspose.Imaging;
+using Aspose.Imaging.ImageOptions;
+using Aspose.Imaging.FileFormats.Tiff.Enums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputPath = "input.png";
+        string outputPath = "output.tif";
+
+        using (Image image = Image.Load(inputPath))
+        {
+            RasterImage rasterImage = (RasterImage)image;
+
+            rasterImage.Filter(rasterImage.Bounds, new Aspose.Imaging.ImageFilters.FilterOptions.GaussianBlurFilterOptions(5, 4.0));
+
+            BigTiffOptions options = new BigTiffOptions(TiffExpectedFormat.Default);
+
+            rasterImage.Save(outputPath, options);
+        }
+    }
+}
