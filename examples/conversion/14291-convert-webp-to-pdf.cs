7bd2d93b015b80e9e5812f6b5a4a1257bb3b26c9
Atir Tahir <atir.tahir@aspose.com>
2026-03-11 01:58:23 +0500

    fix: remove duplicate uppercase folders

--- examples/conversion/14291-convert-webp-to-pdf.cs
+++
@@ -1,9 +0,0 @@
-string dataDir = @"c:\temp\";
-
-// Load a WebP image from a file.
-using (Aspose.Imaging.FileFormats.Webp.WebPImage webPImage = new Aspose.Imaging.FileFormats.Webp.WebPImage(dataDir + "test.webp"))
-{
-    // Save the loaded WebP image to PDF.
-    // Only the active frame is saved because PDF is a single‑page format.
-    webPImage.Save(dataDir + "test.pdf", new Aspose.Imaging.ImageOptions.PdfOptions());
-}
